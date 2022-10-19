a6d0e5a044f153502610c6ec07610ef981f6b46f
jorotodorovv <joro.todorov.96@gmail.com>
2022-10-19 22:27:28 +0300

    Youtube iframe component refactoring

--- my-app/src/api/youtube/iframe.js
+++ my-app/src/api/youtube/iframe.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo } from "react";
 
 const MAX_WIDTH = "100%";
 const MAX_HEIGHT = "100%";
@@ -6,10 +6,8 @@ const MAX_HEIGHT = "100%";
 const REQUEST_SECONDS_DELAY = 0.25;
 
 const YoutubeFrame = (props) => {
-    const [state, setState] = useState();
-
     const getSeconds = (e) => {
-        let time = window.player.getCurrentTime() + REQUEST_SECONDS_DELAY;
+        let time = e.target.getCurrentTime() + REQUEST_SECONDS_DELAY;
         let seconds = Math.ceil(time);
 
         return seconds;
@@ -19,30 +17,33 @@ const YoutubeFrame = (props) => {
         if (props.onReady) {
             props.onReady();
         }
+
+        e.target.preserve = onPreserve.bind(this, e);
     }
 
     const onStateChange = (e) => {
-        setState(state => e.data);
+        switch (e.data) {
+            case YT.PlayerState.PLAYING:
+                if (props.onPlaying) {
+                    props.onPlaying();
+                }
+                break;
+            case YT.PlayerState.ENDED:
+                if (props.onEnd) {
+                    props.onEnd();
+                }
+                break;
+        }
     };
 
     const onPreserve = (e) => {
-        if (props.onPreserve && state !== YT.PlayerState.ENDED) {
+        if (props.onPreserve &&
+            e.target.getPlayerState &&
+            e.target.getPlayerState() === YT.PlayerState.PLAYING) {
             let seconds = getSeconds(e);
             props.onPreserve(e, seconds);
         }
     };
-
-    const onPlaying = (e) => {
-        if (props.onPlaying) {
-            props.onPlaying();
-        }
-    }
-
-    const onEnd = (e) => {
-        if (props.onEnd) {
-            props.onEnd();
-        }
-    }
 
     const opts = useMemo(() => {
         return {
@@ -65,25 +66,17 @@ const YoutubeFrame = (props) => {
     }, [props.seconds]);
 
     useEffect(() => {
-        switch (state) {
-            case YT.PlayerState.PLAYING:
-                onPlaying();
-                break;
-            case YT.PlayerState.ENDED:
-                onEnd();
-                break;
-        }
-
-        return () => {
-            onPreserve();
-        };
-    }, [state])
-
-    useEffect(() => {
         if (YT) {
             window.player =
                 new YT.Player(opts.videoId, opts);
         }
+
+        return () => {
+            if (window.player) {
+                window.player.preserve();
+                window.player.destroy();
+            }
+        };
     }, [YT]);
 
     return <div id={opts.videoId}></div>;
